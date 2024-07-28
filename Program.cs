c4891e3c7d8e6bd5d12153dc43814fecbca5d3be
Prashant Priyadarshi <48068632+PrashantUnity@users.noreply.github.com>
2024-07-28 05:44:25 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿var ls = Enumerable.Range(1,8).Select(i => i.ToString()).ToList();
+
+
+
+
+SaveVideosToFile(ls, "Text.txt");
+static void SaveVideosToFile(List<string> videos, string filePath)
+{
+    using StreamWriter file = new(filePath);
+    foreach (var video in videos)
+    {
+        file.WriteLine(video);
+    }
+}
